2fc98dc5d2492d7343e2b8432667820de6dcf770
Jaroost <antoinejaquet@gmail.com>
2016-05-06 10:46:54 +0200

    Add first component angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    `
+})
+export class AppComponent {
+  title = 'An Angular 2 Force Awakens';
+}
