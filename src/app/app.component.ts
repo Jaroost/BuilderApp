d336c4314e20d3bea1adc162f1abd4f06f09935c
Jaroost <antoinejaquet@gmail.com>
2016-05-10 20:59:01 +0200

    Add first commonent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-    <h1>{{title}}</h1>
-    `
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'An Angular 2 Force Awakens test super cool';
+  test:string[] = ["Hello", 'super', 'cool', 'concept'];
 }
