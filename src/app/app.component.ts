fb0229dfdf44b1cfaf0c5580abaff248edf64e2b
Jaroost <antoinejaquet@gmail.com>
2016-05-11 21:26:09 +0200

    Add Css style for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'An Angular 2 Force Awakens test super cool';
