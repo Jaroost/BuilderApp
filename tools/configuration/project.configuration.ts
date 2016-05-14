916cd9ae03745aabcd4aff8e456047c8a75f8854
Jaroost <antoinejaquet@gmail.com>
2016-05-14 17:26:02 +0200

    Add proxy to browsersync  to the express server on 3002 port
    Add proxy to browsersync to routes request from localhost:3000/api to localhost:3002

--- tools/configuration/project.configuration.ts
+++ tools/configuration/project.configuration.ts
@@ -9,12 +9,33 @@ export class ProjectConfiguration{
 
 	APP_BASE: string = '/';
 
-	BROWSER_SYNC_CONFIG: any = {
+	/*BROWSER_SYNC_CONFIG: any = {
 		middleware: [require('connect-history-api-fallback')({ index: `/index.html` })],
 		port: 3000,
 		startPath: '/',
 		server: {
 			baseDir: `dist/empty`,
+			routes: {
+				['/dist/dev/app']: 'dist/dev/app',
+				['/node_modules']: 'node_modules',
+				['']: 'dist/dev/app'
+			}
+		}
+	};*/
+	
+	
+	BROWSER_SYNC_CONFIG: any = {
+		port: 3000,
+		startPath: '/',
+		server: {
+			baseDir: `dist/empty`,
+			middleware: [require('proxy-middleware')({
+				protocol: 'http:',
+				route: '/api',
+				hostname: 'localhost',
+				port: 3002,
+				}),	
+				require('connect-history-api-fallback')({ index: `/index.html` })],
 			routes: {
 				['/dist/dev/app']: 'dist/dev/app',
 				['/node_modules']: 'node_modules',
