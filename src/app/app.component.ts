e23d9dbefd79a0bcfe9222955116d5e7f8ae0cb8
Jaroost <antoinejaquet@gmail.com>
2016-05-22 17:48:47 +0200

    Add Jwt with Auth0 for the angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {HeroesComponent} from './heroes/heroes.component';
+import {Auth0Component} from './auth0/auth0.component';
 
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css'],
-    directives: [HeroesComponent]
+    directives: [HeroesComponent, Auth0Component]
 })
 export class AppComponent {
   title = 'An Angular 2 Force Awakens test super cool 2';
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,17 @@
 import * as express from 'express';
 import * as util from 'gulp-util';
 import * as chalk from 'chalk';
+import * as jwt from 'express-jwt';
 import {User} from './user';
 
 const EXPRESS_APP = express();
-
+const JWT_CHECK=jwt({
+	secret: new Buffer("VqPEjyiGncpqLZoPFgZNUIQsEcVRbZ4vcjN85uQF5cTaAHCIauUacPTdQ_f0OzZ1", 'base64'),
+	audience: 'bUfoWb8hk8OsP9VUV2gpr7WlFE5LOWb9'
+});
 let users=User.RandomUsers(10000);
 
+EXPRESS_APP.use('/', JWT_CHECK);
 EXPRESS_APP.get('/', (req: express.Request, res: express.Response) => {
 	res.json('Welcome to the builder APP REST API!');
 });
