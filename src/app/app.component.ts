56f9701a926be8af9c2c555cae2f9050149316a8
Antoine Jaquet <antoinejaquet@gmail.com>
2016-05-24 06:54:21 +0200

    Add the router to the angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
 import {Component} from '@angular/core';
-//import {Router,Routes,ROUTER_DIRECTIVES} from '@angular/router';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import {HeroesComponent} from './heroes/heroes.component';
 import {Auth0Component} from './auth0/auth0.component';
+import {ProfileComponent} from './profile/profile.component';
+import {TestComponent} from './test/test.component';
 
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css'],
-    directives: [HeroesComponent, Auth0Component]
+    directives: [HeroesComponent, Auth0Component, ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
+@RouteConfig([
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileComponent
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: TestComponent
+  }
+])
 export class AppComponent {
-  title = 'An Angular 2 Force Awakens test super cool 2';
-  test:string[] = ["Hello", 'super', 'cool', 'concept'];
 }
