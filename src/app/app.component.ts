32ed9ac9d03b630529ff137b1f550adf05eca2d3
Antoine Jaquet <antoinejaquet@gmail.com>
2016-05-25 06:56:14 +0200

    Add login/logout and protect the profile page
    Now the profile page is only accessible when user is logged in and the page show him the profile image and the the name and the assigned groups.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import {Component} from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import {HeroesComponent} from './heroes/heroes.component';
-import {Auth0Component} from './auth0/auth0.component';
 import {ProfileComponent} from './profile/profile.component';
 import {TestComponent} from './test/test.component';
+import { AuthHttp, tokenNotExpired , JwtHelper} from 'angular2-jwt';
+declare var Auth0Lock; //indicate to Typescrit hey man the Auth0Lock is availlable in the cd.auth0.com/js/lock-8.1.min.js!
 
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css'],
-    directives: [HeroesComponent, Auth0Component, ROUTER_DIRECTIVES],
+    directives: [HeroesComponent, ROUTER_DIRECTIVES],
     providers: [ROUTER_PROVIDERS]
 })
 @RouteConfig([
@@ -26,4 +27,35 @@ import {TestComponent} from './test/test.component';
   }
 ])
 export class AppComponent {
+  lock = new Auth0Lock('bUfoWb8hk8OsP9VUV2gpr7WlFE5LOWb9', 'antoinejaquet.eu.auth0.com');
+  jwtHelper: JwtHelper=new JwtHelper();
+  constructor() { }
+
+  ngOnInit() { }
+  
+  login() {
+      this.lock.show((err:string, profile:string, id_token:string)=>{
+          if (err){
+              throw new Error(err);
+          }
+          
+          localStorage.setItem('profile', JSON.stringify(profile));
+          localStorage.setItem('id_token', id_token);
+          
+          console.log(
+              this.jwtHelper.decodeToken(id_token),
+              this.jwtHelper.getTokenExpirationDate(id_token),
+              this.jwtHelper.isTokenExpired(id_token)
+          )
+      });
+  }
+  
+  logout() {
+      localStorage.removeItem('profile');
+      localStorage.removeItem('id_token');
+  }
+
+  loggedIn() {
+      return tokenNotExpired();
+  }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {CanActivate} from '@angular/router-deprecated';
+import {tokenNotExpired} from 'angular2-jwt';
 @Component({
     moduleId: module.id,
     selector: 'profile',
     templateUrl: './profile.component.html'
 })
+@CanActivate(()=>tokenNotExpired())
 export class ProfileComponent implements OnInit {
-    constructor() { }
+    profile: any;
+    
+    constructor() {
+        this.profile=JSON.parse(localStorage.getItem('profile'));
+        console.log(this.profile);
+     }
 
     ngOnInit() { }
 
