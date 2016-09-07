925027612579b9a2aaf3cf620f8ac3fc570a8b09
Jaroost <antoinejaquet@gmail.com>
2016-09-07 20:34:55 +0200

    Add all necessary code for booting the app with npm start

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+
+import { HomeModule } from './home/home.module';
+import {ToolbarModule} from './toolbar/toolbar.module';
+
+@NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(routes), HomeModule, ToolbarModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
